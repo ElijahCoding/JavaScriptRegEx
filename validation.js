d20c2d6b745bd8f7744bd7bf75a44e774bb874f1
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-19 19:42:36 +0300

    all patterns

--- validation.js
+++ validation.js
@@ -1,7 +1,11 @@
 const inputs = document.querySelectorAll('input')
 
 const patterns = {
-    telephone: /^\d{11}$/
+    telephone: /^\d{11}$/,
+    username: /^[a-z\d]{5,12}$/i,
+    password: /^[\d\w@-]{8,20}$/i,
+    slug: /^[a-z\d-]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 }
 
 function validate(field, regex)
